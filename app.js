49e49eeef497693cd886e2d3f6f7ecbb647ddbff
srojas19 <s.rojas19@uniandes.edu.co>
2017-09-18 02:25:56 -0500

    Arreglo

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var index = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'client/build'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
